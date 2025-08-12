31bc5d68dafd2ee3ba6cd0d93ca39c2f20f7822d
Gian Felzon Pascual <gian.pascual@devsoft.ph>
2025-08-12 15:45:06 +0800

    Implement example

---
+++ CS/DiagramBlazorApp/DevExtremeComponents/DevExtremeDiagram.razor.js
@@ -0,0 +1,20 @@
+﻿﻿export async function initializeDiagram(element, dataSource) {
+     const projectTasks = !!dataSource ? dataSource : null;
+
+     return $(element).dxDiagram({
+         nodes: {
+             dataSource: new DevExpress.data.ArrayStore({
+                 key: 'id',
+                 data: projectTasks,
+             }),
+             keyExpr: "id",
+             parentKeyExpr: "parent_ID",
+             textExpr: "task_Name",
+         },
+     });
+}
+
+export async function changeDiagramDataSource(diagram, datasource) {
+    const projectTasks = !!dataSource ? dataSource : null;
+    diagram.import(projectTasks);
+}
